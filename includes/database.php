6dc94c89a4f172ff2628c49f122a9185bc915e57
Dilmohit Singh Bhatha <dillonsingh@hotmail.com>
2025-02-19 22:29:39 +0100

    bijna klaar

--- includes/database.php
+++ includes/database.php
@@ -1,14 +1,11 @@
 <?php
-// Configuratie voor de databaseverbinding
-$host = 'localhost'; // Of je serveradres
-$dbname = 'domeinwinkel'; // De naam van de database
-$username = 'root'; // Je MySQL gebruikersnaam
+$host = 'localhost';
+$dbname = 'domeinwinkel';
+$username = 'root';
 
-// Maak de databaseverbinding
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Verbonden met de database"; // Dit kun je uitschakelen als je wilt
 } catch (PDOException $e) {
     die("Verbinding mislukt: " . $e->getMessage());
 }
